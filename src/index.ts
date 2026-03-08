32aa9a587ab8b405e8431321fca4f2838a83848d
S-Trevizo <strevizo@protonmail.com>
2026-03-08 00:25:01 -0600

    chore: add logs'

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,12 @@ import { app } from "./app.ts";
 
 const PORT = Number(process.env.PORT ?? 4000);
 
+console.log("SONARR_API_KEY at startup:", process.env.SONARR_API_KEY);
+console.log("RADARR_API_KEY at startup:", process.env.RADARR_API_KEY);
+console.log("PROWLARR_API_KEY at startup:", process.env.PROWLARR_API_KEY);
+console.log("LIDARR_API_KEY at startup:", process.env.LIDARR_API_KEY);
+console.log("BAZARR_API_KEY at startup:", process.env.BAZARR_API_KEY);
+
 serve({ fetch: app.fetch, port: PORT, createServer }, () => {
   console.log(`[healthcheck-api] Listening on http://localhost:${PORT}`);
 });
